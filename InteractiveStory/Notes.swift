c404c967997c2dabcd9169cc11fd019649421f1f
Jevaughn Mckenzie <jmckenzie1004@gmail.com>
2017-03-03 11:27:03 -0500

    Story App completed

---
+++ InteractiveStory/Notes.swift
@@ -0,0 +1,67 @@
+//
+//  Notes.swift
+//  InteractiveStory
+//
+//  Created by Jevaughn McKenzie on 3/3/17.
+//  Copyright © 2017 Treehouse Island. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ ------ Topics covered in this example ------
+ 
+ Navigation Controllers
+    Hierarchical data
+    Segues
+    Multiple screens of information
+ 
+ Custom Data Strutures
+    Linked List
+    Trees
+    Efficient Data Structures
+ 
+ Programmatic Views
+ 
+ Communication Patters
+    Delegates
+    Notifications
+ 
+ Fun Stuff
+    Accepting text input
+    Playing sound
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
